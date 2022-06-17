cc005046a22f59ccfe1a612bb9e29d940938df37
xXxBOBAxXx <100014195+xXxBOBAxXx@users.noreply.github.com>
2022-06-17 19:41:19 +0300

    1

--- from_phonebook.py
+++ from_phonebook.py
@@ -1,6 +1,12 @@
 import csv
-phonebook = []
-with open("phonebook.csv", encoding = 'utf-8') as file:
-    reader = csv.reader(file, delimiter = "|")
-    for id in reader:
-        phonebook.append(id)
+
+pb = []
+
+def from_pb(pb):
+    with open("phonebook.csv", encoding = 'utf-8') as file:
+        reader = csv.reader(file, delimiter = "|")
+        for id in reader:
+            pb.append(id)
+        return pb
+
+from_pb(pb)
