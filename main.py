2ecf6302be41a33c06308f30f61e1cad95deb54b
FA-lex <a.good.lex@gmail.com>
2022-06-18 14:02:17 +0800

    add main

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import interface as inter
-import print_contacts as contacts
-import find_phonebook
+import controller
 
-command = True
-while command:
-    command = inter.main_menu()
-    if command == 1: contacts.print_all_contacts()
-    elif command == 2:
-        contact = find_phonebook(inter())
-        contacts.print_contact(contact)
-        
+controller.run_programm()
